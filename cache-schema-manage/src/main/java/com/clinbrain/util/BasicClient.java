b5555cebf280d7088ec7193553ea933801edfd04
1016465324 <1016465324@qq.com>
2020-04-23 10:38:22 +0800

    添加基础数据源连接

---
+++ cache-schema-manage/src/main/java/com/clinbrain/util/BasicClient.java
@@ -0,0 +1,26 @@
+package com.clinbrain.util;
+
+import java.util.List;
+
+/**
+ * 基础客户端类
+ */
+public class BasicClient {
+
+    public List<String> getDBNames() {
+        return null;
+    }
+
+    public List<String> getTableNames(String database) {
+        return null;
+    }
+
+    public List<Object> getTableMetas(String database, String tableName) {
+        return null;
+    }
+
+    public List<String> qury(String database) {
+        return null;
+    }
+
+}
--- cache-schema-manage/src/main/java/com/clinbrain/util/CacheClient.java
+++ cache-schema-manage/src/main/java/com/clinbrain/util/CacheClient.java
@@ -1,17 +1,36 @@
 package com.clinbrain.util;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.List;
 
-public class CacheClient {
+public class CacheClient extends BasicClient{
     private static final String driverName = "com.intersys.jdbc.CacheDriver";
 
     public static Connection getConne(String url, String username, String password) throws Exception {
-        Class.forName(driverName);
-        Connection connection = DriverManager.getConnection(url, username, password);
-        return connection;
+        //Class.forName(driverName);
+        //Connection connection = DriverManager.getConnection(url, username, password);
+        DataSource connection = DBUtils.getDataSouce(driverName, url, username, password);
+        return connection.getConnection();
     }
 
+    @Override
+    public List<String> getDBNames() {
+        return super.getDBNames();
+    }
 
+    @Override
+    public List<String> getTableNames(String database) {
+        return super.getTableNames(database);
+    }
 
+    @Override
+    public List<Object> getTableMetas(String database, String tableName) {
+        return super.getTableMetas(database, tableName);
+    }
+
+    @Override
+    public List<String> qury(String database) {
+        return super.qury(database);
+    }
 }
