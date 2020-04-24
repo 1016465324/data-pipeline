634a1d6a1a295a0cade046b268e4369e785c8bf9
e <micle-qin@live.com>
2020-04-24 14:50:39 +0800

    添加日期时间转换工具类

---
+++ cache-schema-manage/src/main/java/com/clinbrain/util/DateTimeUtil.java
@@ -0,0 +1,70 @@
+package com.clinbrain.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class DateTimeUtil {
+
+    private static Logger logger= LoggerFactory.getLogger(DateTimeUtil.class);
+    public static final String dateString = "1841-01-01";
+
+    public static Date IntToDate(int day) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date Converted=new Date();
+        try {
+            Date date =sdf.parse(dateString);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            calendar.add(Calendar.DATE,day-1);
+            Converted=calendar.getTime();
+
+        } catch (ParseException e) {
+            logger.info("IntToDate failed: {}", e);
+
+        }
+        return Converted;
+
+    }
+
+    public  static String IntToShortDate(int day){
+        Date date=IntToDate(day);
+        String formatDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+        return formatDate;
+
+    }
+
+    public  static String  IntToTime(int second){
+        int sec=second%60;
+        int totMin=second/60;
+        int min=totMin%60;
+        int hours=totMin/60;
+        String time="";
+        return time.format("%s:%s:%s",ToTime(hours),ToTime(min),ToTime(sec));
+
+    }
+
+    public  static String ToTime(int num){
+        String time="";
+        if (num<10){
+            return time.format("0%s",num);
+        }
+        return time.format("%s",num);
+
+    }
+
+
+    //2016-08-03 00:00:00.000 64133
+    public static void main(String[] args){
+        String date =IntToShortDate(64243);
+        String time=IntToTime(32761);
+        System.out.println(date);
+        System.out.println(time);
+    }
+}
